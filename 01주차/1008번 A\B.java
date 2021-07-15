d2e3f5006104860ea568e62330f36a19f4bca0ce
KimHyunsoo <tomy9729@naver.com>
2021-07-15 18:56:30 +0900

    Create 1008번 A\B.java

---
+++ 01주차/1008번 A\B.java
@@ -0,0 +1,11 @@
+//1008번 A/B.java
+import java.util.Scanner;
+
+public class A_B_1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println((double)((double)a/(double)b));
+	}
+}
