763225b941a21ce874b5f10c0742558cd2c581eb
KimHyunsoo <tomy9729@naver.com>
2021-07-15 18:51:39 +0900

    Create 1001번 A-B.java

---
+++ 01주차/1001번 A-B.java
@@ -0,0 +1,10 @@
+//1001번 A-B.java
+import java.util.Scanner;
+public class AminusB_1001 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a-b);
+	}
+}
