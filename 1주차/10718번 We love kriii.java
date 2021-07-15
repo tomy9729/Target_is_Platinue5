c720faec98f6522e4d3229f7e38a823ff0352f5c
KimHyunsoo <tomy9729@naver.com>
2021-07-15 18:48:53 +0900

    Delete 1주차 directory

--- 1주차/10718번 We love kriii.java
+++
@@ -1,7 +0,0 @@
-//10718번 We love kriii.java
-public class Welovekeriii_10718 {
-	public static void main(String[] args) {
-		System.out.println("강한친구 대한육군");
-		System.out.println("강한친구 대한육군");
-	}
-}
