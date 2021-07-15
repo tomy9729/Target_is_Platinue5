12a3c5b7441ea6eb8ae852e700c7f6967a5fd253
KimHyunsoo <tomy9729@naver.com>
2021-07-15 18:53:08 +0900

    Create 10998번 AxB.java

---
+++ 01주차/10998번 AxB.java
@@ -0,0 +1,11 @@
+//10998번 AxB.java
+import java.util.Scanner;
+
+public class AxB_10998 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a*b);
+	}
+}
