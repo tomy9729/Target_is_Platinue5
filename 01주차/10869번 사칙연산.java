d15cdf11eea95f39521c2ab85dca124f7fbf1eb1
KimHyunsoo <tomy9729@naver.com>
2021-07-15 18:59:29 +0900

    Create 10869번 사칙연산.java

---
+++ 01주차/10869번 사칙연산.java
@@ -0,0 +1,15 @@
+//10869번 사칙연산.java
+import java.util.Scanner;
+
+public class 사칙연산_10869 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
