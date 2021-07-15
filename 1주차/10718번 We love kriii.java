ef1fe7a1971b12b96244a257c15364d13316466c
KimHyunsoo <tomy9729@naver.com>
2021-07-15 18:28:30 +0900

    Create 10718번 We love kriii.java

---
+++ 1주차/10718번 We love kriii.java
@@ -0,0 +1,7 @@
+//10718번 We love kriii.java
+public class Welovekeriii_10718 {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
