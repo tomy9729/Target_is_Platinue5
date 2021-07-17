eb753f58c4a144c9cf0b9e948a9234eac7bac2c8
KimHyunsoo <tomy9729@naver.com>
2021-07-17 22:31:55 +0900

    Create 2741번 N찍기.java

---
+++ 01주차/2741번 N찍기.java
@@ -0,0 +1,12 @@
+//2741번 N찍기.java
+import java.util.Scanner;
+
+public class N찍기_2741 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=1;i<=N;i++) {
+			System.out.println(i);
+		}
+	}
+}
