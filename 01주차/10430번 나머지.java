18abb27c610d79a49f9f5ead33d7a47bb4145ca5
KimHyunsoo <tomy9729@naver.com>
2021-07-17 22:15:04 +0900

    Create 10430번 나머지.java

---
+++ 01주차/10430번 나머지.java
@@ -0,0 +1,16 @@
+//10430번 나머지.java
+import java.util.Scanner;
+
+public class 나머지_10430 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C)+(B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C)*(B%C))%C);
+	}
+}
