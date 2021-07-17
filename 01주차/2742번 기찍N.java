5ab51d54747cf0432fb94abb6ca161b6f2dabfd6
KimHyunsoo <tomy9729@naver.com>
2021-07-17 22:33:46 +0900

    Create 2742번 기찍N.java

---
+++ 01주차/2742번 기찍N.java
@@ -0,0 +1,12 @@
+//2742번 기찍N.java
+import java.util.Scanner;
+
+public class 기찍N_2742 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = N;i>0;i--) {
+			System.out.println(i);
+		}
+	}
+}
